dfbe1ffa557fdecf2e9d5a063854d61f0da146d9
Anton Polyakov <polyakov.anton@ukr.net>
2020-12-03 21:16:25 +0200

    Lesson 12

--- lesson_11/bot.py
+++ lesson_11/bot.py
@@ -1,5 +1,7 @@
 from config import TOKEN
 from telebot import TeleBot
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+import json
 import random
 
 bot = TeleBot(TOKEN)
@@ -7,19 +9,44 @@ bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    print(message)
+    kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button1 = KeyboardButton('Привет', request_contact=True)
+    button2 = KeyboardButton('Хай', request_location=True)
+    button3 = KeyboardButton('Салют')
+
+    kb.add(button1, button2, button3)
+
     bot.send_message(
         message.chat.id,
-        'Hello!'
+        'Hello!',
+        reply_markup=kb
     )
 
 
 @bot.message_handler(func=lambda m: m.text in {'Привет', 'Хай', 'Салют'})
 def handle_hello(message):
     greetings = ['Hola', 'Hello', 'Hi', 'Здравствуй']
+    kb = InlineKeyboardMarkup()
+
+    json_data = json.dumps({
+        'tag': 'Hi',
+        'id': '1234'
+    })
+    print(json_data)
+    button1 = InlineKeyboardButton('Привет', callback_data=json_data)
+
+    json_data = json.dumps(
+        {
+            'tag': 'Bye',
+            'id': '5678'
+        }
+    )
+    button2 = InlineKeyboardButton('Пока', callback_data=json_data)
+    kb.add(button1, button2)
     bot.send_message(
         message.chat.id,
-        random.choice(greetings)
+        random.choice(greetings),
+        reply_markup=kb
     )
 
 
@@ -38,6 +65,21 @@ def handle_whoami(message):
                 f'Никнейм: @{message.from_user.username}\nid: {message.from_user.id}'
     bot.send_message(message.chat.id, user_info)
 
+
+@bot.callback_query_handler(func=lambda c: json.loads(c.data).get('tag') == 'Hi')
+def handle_inline(call):
+    bot.send_message(
+        call.message.chat.id,
+        'HI!'
+    )
+
+
+@bot.callback_query_handler(func=lambda c: json.loads(c.data).get('tag') == 'Bye')
+def handle_inline(call):
+    bot.send_message(
+        call.message.chat.id,
+        'BYE!'
+    )
 
 # import time
 # for i in range(10):
